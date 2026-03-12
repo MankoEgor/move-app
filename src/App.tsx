10411d3b7dc0ebc3ccf56a59931df651f98997c3
Egor Manko <mankoegor5@gmail.com>
2026-03-12 10:38:29 +0300

    create NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import {Route, Routes} from "react-router-dom";
+import SearchPage from "./pages/SearchPage";
+import MoviePage from "./pages/MoviePage";
+import FavoritesPage from "./pages/FavoritesPage";
+
 function App() {
   return(
-    <div>  
-      <h1>CINE</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<SearchPage/>}/>;
+      <Route path="/movie/:id" element={<MoviePage/>}/>;
+      <Route path="/favorites" element={<FavoritesPage/>}/>;
+    </Routes>
   );
 }
 
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,25 @@
+import { Link, useLocation } from "react-router-dom";
+
+function NavBar(){
+    let location = useLocation();
+
+    return(
+        <nav className="navbar">
+            <div className="logo"></div>
+            <ul className="nav-links">
+                <li>
+                    <Link 
+                    to="/"
+                    className={location.pathname === "/" ? "active" : ""}>ПОИСК</Link>
+                </li>
+                <li>
+                    <Link 
+                    to="/favorites"
+                    className={location.pathname === "/favorites" ? "active" : ""}>ИЗБРАННОЕ</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar;
