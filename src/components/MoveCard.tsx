a334a19380aefc85227d738a21afa57968d43f58
Egor Manko <mankoegor5@gmail.com>
2026-03-12 23:47:23 +0300

    add function, that provide users add films to favorite list

--- src/components/MoveCard.tsx
+++ src/components/MoveCard.tsx
@@ -1,5 +1,5 @@
 import {useNavigate} from "react-router-dom";
-
+import {useFavorite} from "../context/FavoritesContext";
 import {IMAGE_BASE_URL} from "../api/tmdb.ts";
 
 import "../styles/movieCard.css";
@@ -13,11 +13,21 @@ interface movieCardProps{
 
 function MovieCard({id, title, poster_path, vote_average}: movieCardProps){
     const navigate = useNavigate();
-
+    const {isFavorite, favoriteToggle} = useFavorite();
     return(
-        <div className="movie-card" style={{backgroundImage: `url(${IMAGE_BASE_URL}${poster_path})`}} onClick={() => navigate(`/movie/${id}`)}>
+        <div className="movie-card" onClick={() => navigate(`/movie/${id}`)}>
+            {poster_path 
+            ? <img src={`${IMAGE_BASE_URL}${poster_path}`} alt={title} /> 
+            : <div>No Image</div>}
             <p>{title}</p>
             <p>{vote_average}</p>
+            <button onClick={
+                (e) => {
+                    e.stopPropagation()
+                    favoriteToggle(id)
+                }}>
+                {isFavorite(id) ? '♥' : '♡'}
+                </button>
         </div>
     );
 }
