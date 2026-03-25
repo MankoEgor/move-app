89e4c9f1853e2e780269b19cfa178125ce479ddf
Egor Manko <mankoegor5@gmail.com>
2026-03-25 09:13:46 +0300

    add ErrorBoundery in project

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import {FavoritesProvider, GenresProvoder} from "./context/FavoritesContext";
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.tsx';
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
@@ -9,7 +10,9 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <FavoritesProvider>
         <GenresProvoder>
-          <App />
+          <ErrorBoundary>
+            <App />
+          </ErrorBoundary>
         </GenresProvoder>
       </FavoritesProvider>
     </BrowserRouter>
